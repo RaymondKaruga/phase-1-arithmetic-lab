0f1e1aab6cd8b3983ed2afb2ddfcbdfc48de6b99
RaymondKaruga <raymond.karuga@student.moringaschool.com>
2024-03-20 15:56:08 -0700

    Adds index.js

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1=31
+let num2=2
+let multiply=num1*num2
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1
 
-// Write your code here
+let num3=10
+let num4=6
+let mod=num3%num4
+
+const set = [2, 5, 20, 10]
+const max = Math.max(...set)
+
+
+
